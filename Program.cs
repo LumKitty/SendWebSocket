16ea96be7fc62962925a2ec700f1dd9eb2d99000
lum <lum@LINA>
2024-03-17 02:22:46 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.WebSockets;
+using WatsonWebsocket;
+String message = string.Join(" ", args).Substring(args[0].Length + 1);
+WatsonWsClient wsClient = new WatsonWsClient(new Uri(args[0]));
+System.Threading.CancellationToken CT = new System.Threading.CancellationToken();
+wsClient.KeepAliveInterval = 1000;
+wsClient.Start();
+wsClient.SendAsync(message, WebSocketMessageType.Text, CT);
+wsClient.Stop();
